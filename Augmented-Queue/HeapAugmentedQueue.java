3e97defef84f7a8cdec1188ca9805213f38b800b
Christopher Roberts <chrisroberts1109@gmail.com>
2021-05-12 17:02:26 -0400

    Update HeapAugmentedQueue.java

--- Augmented-Queue/HeapAugmentedQueue.java
+++ Augmented-Queue/HeapAugmentedQueue.java
@@ -1,124 +1,116 @@
-import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class HeapAugmentedQueue implements IAugmentedQueue {
-	private int[] queue = new int[1];
-	private final AugmentedHeap minHeap = new AugmentedHeap(true);
-	private final AugmentedHeap maxHeap = new AugmentedHeap(false);
-	private int front = 0;
-	private int size = 0;
+    private int[] queue = new int[1];
+    private int front = 0;
+    private int size = 0;
+    private AugmentedHeap minHeapAllValues = new AugmentedHeap(true);
+    private AugmentedHeap maxHeapAllValues = new AugmentedHeap(false);
+    private AugmentedHeap maxHeapSmallValues = new AugmentedHeap(false);
+    private AugmentedHeap minHeapBigValues = new AugmentedHeap(true);
+    
+    @Override
+    public int size() {
+        return size;
+    }
 
-	@Override
-	public int size() {
-		return size;
-	}
+    @Override
+    public void enqueue(int value) {
+        if (size == queue.length) resize();
+        queue[getIndex(front + size)] = value;
+        minHeapAllValues.add(value);
+        maxHeapAllValues.add(value);
+        
+        if (maxHeapSmallValues.size() == 0 && minHeapBigValues.size() == 0) 
+            maxHeapSmallValues.add(value);
+        else if (maxHeapSmallValues.size() == 0 || value > maxHeapSmallValues.top()) 
+            minHeapBigValues.add(value);
+        else 
+            maxHeapSmallValues.add(value);
+        size++;
+    }
 
-	@Override
-	public void enqueue(int value) {
-		if (size == queue.length) resize();
-		queue[getIndex(front + size)] = value;
-		minHeap.add(value);
-		maxHeap.add(value);
-		size++;
-	}
+    @Override
+    public int dequeue() {
+        if (size == 0) throw new NoSuchElementException();
+        int retVal = queue[front];
+        front = getIndex(front+1);
+        size--;
+        
+        minHeapAllValues.remove(retVal);
+        maxHeapAllValues.remove(retVal);
+        if (maxHeapSmallValues.size() > 0 && retVal <= maxHeapSmallValues.top())
+            maxHeapSmallValues.remove(retVal);
+        else
+            minHeapBigValues.remove(retVal);
+        return retVal;
+    }
 
-	@Override
-	public int dequeue() {
-		if (size == 0) throw new NoSuchElementException();
-		int retVal = queue[front];
-		front = getIndex(front+1);
-		size--;
-		minHeap.deleteTop();
-		maxHeap.deleteTop();
-		return retVal;
-	}
+    @Override
+    public int front() {
+        if (size == 0)
+            throw new NoSuchElementException();
+        return queue[front];
+    }
 
-	@Override
-	public int front() {
-		if (size == 0)
-			throw new NoSuchElementException();
-		return queue[front];
-	}
+    @Override
+    public int back() {
+        if (size == 0)
+            throw new NoSuchElementException();
 
-	@Override
-	public int back() {
-		if (size == 0)
-			throw new NoSuchElementException();
+        return queue[getIndex(front + size - 1)];
+    }
 
-		return queue[getIndex(front + size - 1)];
-	}
+    @Override
+    public int median() {
+        if (size == 0)
+            throw new NoSuchElementException();
 
-	@Override
-	public int median() {
-		if (size == 0)
-			throw new NoSuchElementException();
+        while (maxHeapSmallValues.size() > minHeapBigValues.size()) {
+            minHeapBigValues.add(maxHeapSmallValues.deleteTop());
+        }
 
-		AugmentedHeap minTopValue = new AugmentedHeap(false);
-		AugmentedHeap maxTopValue = new AugmentedHeap(true);
-		int maxSize = maxTopValue.size();
-		int minSize = minTopValue.size();
-		for(int i = 0; i < size; i++){
-			minHeap.add(queue[i]);
-		}
-		for (int i = 0; i<size; i++){
-			if(minHeap.top() > queue[i]){
-				minTopValue.add(queue[i]);
-			}
-		}
-		for(int i = 0; i < size; i++){
-			maxHeap.add(queue[i]);
-		}
-		for (int i = 0; i<size; i++){
-			if(maxHeap.top() < queue[i]){
-				maxTopValue.add(queue[i]);
-			}
-		}
+        while (maxHeapSmallValues.size() + 1 < minHeapBigValues.size()) {
+            maxHeapSmallValues.add(minHeapBigValues.deleteTop());
+        }
+        
+        if (minHeapBigValues.size() == maxHeapSmallValues.size()) {
+            return (minHeapBigValues.top() + maxHeapSmallValues.top()) / 2;
+        }
+        else {
+            return minHeapBigValues.top();
+        }
+    }
 
-		if (minSize > maxSize) {
-			return minTopValue.top();
-		}
-		if (minSize < maxSize){
-			return maxTopValue.top();
-		}
+    @Override
+    public int min() {
+        if (size == 0)
+            throw new NoSuchElementException();
 
-		return (minTopValue.top() + maxTopValue.top())/2;
-	}
+        return minHeapAllValues.top();
+    }
 
-	@Override
-	public int min() {
-		if (size == 0)
-			throw new NoSuchElementException();
-		AugmentedHeap minHeap = new AugmentedHeap(true);
-		for(int i = 0; i < size; i++){
-			minHeap.add(queue[i]);
-		}
-		return minHeap.top();
-	}
+    @Override
+    public int max() {
+        if (size == 0)
+            throw new NoSuchElementException();
 
-	@Override
-	public int max() {
-		if (size == 0)
-			throw new NoSuchElementException();
-		AugmentedHeap maxHeap = new AugmentedHeap(false);
-		for(int i = 0; i < size; i++){
-			maxHeap.add(queue[i]);
-		}
-		return maxHeap.top();
-	}
+        return maxHeapAllValues.top();
+    }
 
-	private void resize() {
-		int[] biggerQueue = new int[queue.length * 2];
-		
-		for (int i = 0; i < size; i++) {
-			biggerQueue[i] = queue[getIndex(front + i)];
-		}
-		queue = biggerQueue;
-		front = 0;
-	}
-	
-	private int getIndex(int x) {
-		x = x % queue.length;
-		if (x < 0) x += queue.length;
-		return x;
-	}
+    private void resize() {
+        int[] biggerQueue = new int[queue.length * 2];
+        
+        for (int i = 0; i < size; i++) {
+            biggerQueue[i] = queue[getIndex(front + i)];
+        }
+        queue = biggerQueue;
+        front = 0;
+    }
+    
+    private int getIndex(int x) {
+        x = x % queue.length;
+        if (x < 0) x += queue.length;
+        return x;
+    }
 }
